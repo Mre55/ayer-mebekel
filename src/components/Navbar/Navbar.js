562f32c91acf9849adf4b82c1a5348a4c34a4811
mre55 <misganawmihreteab>
2022-01-28 23:12:07 +0300

    Install tailwind css and customize the nav bar and remove unused .css files

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import styles from './Navbar.module.css';
-
 const Navbar = () => {
   const links = [
     {
@@ -17,12 +15,12 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbarLeft}>
-        <h2>Air pollution</h2>
-        <ul className={styles.navbar_list}>
+    <nav className="mb-5">
+      <div className="flex items-center justify-between pl-2 ring-4">
+        <h2 className="text-3xl font-bold text-sky-500">Air pollution</h2>
+        <ul className="flex justify-between pr-8 w-40">
           {links.map((link) => (
-            <li className={styles.linkTexts} key={link.id}>
+            <li className="text-base" key={link.id}>
               <Link to={link.path}>{link.text}</Link>
             </li>
           ))}
