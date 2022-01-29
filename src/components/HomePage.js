75849ae1c320630957084601872e60268830f961
mre55 <misganawmihreteab>
2022-01-29 22:14:45 +0300

    Renders data to the page by accessing from the redux store

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,21 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchWeather } from "../redux/weather/weatherReducer";
+import store from "../redux/configureStore";
 
 function HomePage() {
-  
-  const initialState = {
-    coord: {
-      lon: 50,
-      lat: 50,
-    },
-    list: [
-      {
-        main: {
-          aqi: 1,
-        },
-        components: {
-          co: 277.04,
-          no: 0,
-          no2: 0.38,
-          o3: 77.96,
-          so2: 0.95,
-          pm2_5: 1.57,
-          pm10: 1.62,
-          nh3: 0.09,
-        },
-        dt: 1643400000,
-      },
-    ],
-  };
+  const weatherFromStore = useSelector((state) => state.weatherReducer);
 
-  const dataKeys = Object.keys(initialState);
-  const coord = initialState[dataKeys[0]];
-  const list = initialState[dataKeys[1]][0];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!(store.getState().weatherReducer.length > 0)) {
+      dispatch(fetchWeather());
+    }
+  }, []);
+
+  let { coord = {}, list = [] } = weatherFromStore;
 
   return (
     <div className="pl-5">
@@ -49,56 +35,53 @@ function HomePage() {
         </p>
         <br />
         <h5>Date and time, Unix, UTC</h5>
-        <p>
-          Date and time is :
-          {list.dt}
-        </p>
+        <p>Date and time is :{list.map((listData) => listData?.dt)}</p>
         <br />
         <h5> Air Quality Index.</h5>
         <p>
           Air Quality Index is:
-          {list.main.aqi}
+          {list.map((listData) => listData.main?.aqi)}
         </p>
         <br />
         <h5>Data polluting gases concentration of:</h5>
         <p>
           CO(Carbon monoxide):
-          {list.components.co}
+          {list.map((listData) => listData.components?.co)}
           μg/m3
         </p>
         <p>
           NO(Nitrogen monoxide):
-          {list.components.no}
+          {list.map((listData) => listData.components?.no)}
           μg/m3
         </p>
         <p>
           NO2(Nitrogen dioxide):
-          {list.components.no2}
+          {list.map((listData) => listData.components?.no2)}
           μg/m3
         </p>
         <p>
           O3(Ozone):
-          {list.components.o3}
+          {list.map((listData) => listData.components?.o3)}
           μg/m3
         </p>
         <p>
           SO2(Sulphur dioxide):
-          {list.components.so2}
+          {list.map((listData) => listData.components?.so2)}
           μg/m3
         </p>
         <p>
           PM2.5(Fine particles matter):
-          {list.components.pm2_5}
+          {list.map((listData) => listData.components?.pm2_5)}
           μg/m3
         </p>
         <p>
           PM10(Coarse particulate matter):
-          {list.components.pm10}
+          {list.map((listData) => listData.components?.pm10)}
           μg/m3
         </p>
         <p>
           NH3(Ammonia):
-          {list.components.nh3}
+          {list.map((listData) => listData.components?.nh3)}
           μg/m3
         </p>
       </div>
--- src/redux/weather/weatherReducer.js
+++ src/redux/weather/weatherReducer.js
@@ -1,36 +1,56 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = "FETCH_WEATHER";
 
-export const fetchWeatherSuccess = (weathers) => ({
+export const fetchWeatherSuccess = (payload) => ({
   type: FETCH_WEATHER,
-  payload: weathers,
+  payload,
 });
 
-export const fetchWeather = () => (dispatch) => {
-  axios
-    .get('http://api.openweathermap.org/data/2.5/air_pollution?lat=60&lon=70&appid=6ecf420a1bbd4a7b6cb83e24509c64c2')
-    .then((response) => {
-      console.log('weather API response is ', response.data);
-      const weathers = response.data;
-      dispatch(fetchWeatherSuccess(weathers));
-    });
-};
-
 const initialState = {
-  weather: [],
+  coord: {
+    lon: 38.7578,
+    lat: 8.9806,
+  },
+  list: [
+    {
+      main: {
+        aqi: 5,
+      },
+      components: {
+        co: 2429.96,
+        no: 0,
+        no2: 12.68,
+        o3: 32.9,
+        so2: 6.2,
+        pm2_5: 81.17,
+        pm10: 126.27,
+        nh3: 40.53,
+      },
+      dt: 1643482800,
+    },
+  ],
 };
 
-const reducer = (state = initialState, action) => {
+export const fetchWeather = () => (dispatch) => {
+  // axios
+  //   .get('http://api.openweathermap.org/data/2.5/air_pollution?lat=60&lon=70&appid=6ecf420a1bbd4a7b6cb83e24509c64c2')
+  //   .then((response) => {
+  //     console.log('weather API response is ', response.data);
+  //     const weathers = response.data;
+  //     dispatch(fetchWeatherSuccess(weathers));
+  //   });
+  dispatch(fetchWeatherSuccess(initialState));
+  console.log("weather from reducer is ", initialState);
+};
+
+const weatherReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_WEATHER:
-      return {
-        ...state,
-        weather: action.payload,
-      };
+      return [...state, action.payload][0];
     default:
       return state;
   }
 };
 
-export default reducer;
+export default weatherReducer;
